c8e9813325071fd675b44737cb64b7b0650a151d
Sparsh <sparshgupta2810@gmail.com>
2023-09-26 00:45:18 +0530

    Project Completed !!!

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+var audio = document.createElement('audio');
+audio.setAttribute('scr','sound.mp3/:1');
+audio.loop = true;
+audio.play();
+// let play = document.getElementById('container')
+// function Sound(){
+//     let audio = Audio('sound2.wav');
+//     audio.play
+
+// }
